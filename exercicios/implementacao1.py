c6e64c45c63b30f2600c47f56e0a613494397807
Lucas Neves <lucas54neves@gmail.com>
2019-09-07 00:32:45 -0300

    Criação da classe Vertice

--- exercicios/implementacao1.py
+++ exercicios/implementacao1.py
@@ -9,6 +9,34 @@ Grupo:
 Data: 06/09/2019  
 '''
 
+class Vertice:
+    def __init__(self, x):
+        self.id = x
+        self.cor = "branco"
+        self.distancia = float("inf")
+        self.predecessor = None
+    
+    def set_cor(self, nova_cor):
+        self.cor = nova_cor
+    
+    def set_distancia(self, nova_distancia):
+        self.distancia = nova_distancia
+    
+    def set_predecessor(self, novo_predecessor):
+        self.predecessor = novo_predecessor
+    
+    def get_id(self):
+        return self.id
+    
+    def get_cor(self):
+        return self.cor
+    
+    def get_distancia(self):
+        return self.distancia
+    
+    def get_predecessor(self):
+        return self.predecessor
+
 class Pilha(object):
     def __init__(self):
         self.dados = []
