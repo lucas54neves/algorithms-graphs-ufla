235b10e3f4051c3a2dbb3d10f722d0b533353a93
Lucas Neves <lucas54neves@gmail.com>
2019-09-07 17:49:24 -0300

    Aprimoramento do código e nova versão para a abordagem 1

--- exercicios/implementacao1.py
+++ exercicios/implementacao1.py
@@ -38,7 +38,6 @@ class Vertice:
         self.cor = "branco"
         self.distancia = float("inf")
         self.predecessor = None
-        self.dominante = None
         self.f = float("inf")
     
     def set_cor(self, nova_cor):
@@ -49,9 +48,6 @@ class Vertice:
     
     def set_predecessor(self, novo_predecessor):
         self.predecessor = novo_predecessor
-    
-    def set_dominante(self, novo_dominante):
-        self.dominante = novo_dominante
     
     def set_f(self, novo_f):
         self.f = novo_f
@@ -67,9 +63,6 @@ class Vertice:
     
     def get_predecessor(self):
         return self.predecessor
-    
-    def get_dominante(self):
-        return self.dominante
     
     def get_f(self):
         return self.f
@@ -98,13 +91,12 @@ def leGrafo():
         while valores != []:
             adjacente = valores.pop(0)
             if adjacente != '-':
-                grafo.get_lista_adj()[vertice.get_id()].append(Vertice(int(adjacente)))
+                grafo.get_lista_adj()[vertice.get_id()].append(int(adjacente))
         
         linha = arquivo.readline()
     
-    vertice_inicial = Vertice(0)
     abordagem1(grafo)
-    abordagem2(grafo, vertice_inicial)
+    abordagem2(grafo)
 
 def dfs(grafo):
     for i in range(grafo.get_quantidade_vertices()):
@@ -124,71 +116,75 @@ def dfsVisit(grafo, id_vertice):
     
     for i in range(len(grafo.get_lista_adj()[id_vertice])):
         # Pega o id do vertice adjacente e confere os dados desse vertice na lista de adjacencia
-        if grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i].get_id()].get_cor() == "branco":
-            grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i].get_id()].set_predecessor(grafo.get_lista_vertices()[id_vertice].get_id())
-            dfsVisit(grafo, grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i].get_id()].get_id())
+        if grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].get_cor() == "branco":
+            grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].set_predecessor(grafo.get_lista_vertices()[id_vertice].get_id())
+            dfsVisit(grafo, grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].get_id())
     
     grafo.get_lista_vertices()[id_vertice].set_cor("preto")
     tempo = tempo + 1
     grafo.get_lista_vertices()[id_vertice].set_f(tempo)
 
-# Imprime os vertices dominantes usando DFS
-def abordagem1(grafo):
-    dfs(grafo)
-    
-    print("Abordagem 1 - DFS")
-    #for i in range(grafo.get_quantidade_vertices()):
-        #print(grafo.get_lista_vertices()[i].get_distancia(), grafo.get_lista_vertices()[i].get_f())
-    
+def bfs(grafo, id_fonte):
     i = 0
-    while i < (grafo.get_quantidade_vertices() - 1):
-        j = i + 1
-        while j < (grafo.get_quantidade_vertices()):
-            if grafo.get_lista_vertices()[i].get_distancia() < grafo.get_lista_vertices()[j].get_distancia() and grafo.get_lista_vertices()[i].get_f() > grafo.get_lista_vertices()[j].get_f():
-                grafo.get_lista_vertices()[j].set_dominante(i)
-            j = j + 1
+    while i < grafo.get_quantidade_vertices():
+        if i != id_fonte:
+            grafo.get_lista_vertices()[i].set_cor("branco")
+            grafo.get_lista_vertices()[i].set_distancia(float("inf"))
+            grafo.get_lista_vertices()[i].set_predecessor(None)
         i = i + 1
     
-    for k in range(grafo.get_quantidade_vertices()):
-        print("Vertice {} - Dominante {}".format(grafo.get_lista_vertices()[k].get_id(), grafo.get_lista_vertices()[k].get_dominante()))
-
-# Imprime os vertices dominantes usando BFS
-def abordagem2(grafo, fonte):
-    predecessores = []
-    for i in range(grafo.get_quantidade_vertices()):
-        predecessores.append([])
-    
-    fonte.set_cor("cinza")
-    fonte.set_distancia(0)
+    grafo.get_lista_vertices()[id_fonte].set_cor("cinza")
+    grafo.get_lista_vertices()[id_fonte].set_distancia(0)
+    grafo.get_lista_vertices()[id_fonte].set_predecessor(None)
     
     q = deque()
     
-    q.append(fonte)
+    q.append(id_fonte)
     
     while len(q) > 0:
         u = q.popleft()
         
         i = 0
-        for vertice in grafo.get_lista_adj()[u.get_id()]:
-            v = grafo.get_lista_adj()[u.get_id()][i]
-            if v.get_cor() == "branco":
-                v.set_cor("cinza")
-                v.set_distancia(u.get_distancia()+1)
-                v.set_predecessor(u)
-                predecessores[v.get_id()].append(u.get_id())
+        for vertice_adjacente in grafo.get_lista_adj()[u]:
+            v = grafo.get_lista_adj()[u][i]
+            
+            if grafo.get_lista_vertices()[v].get_cor() == "branco":
+                grafo.get_lista_vertices()[v].set_cor("cinza")
+                grafo.get_lista_vertices()[v].set_distancia(grafo.get_lista_vertices()[u].get_distancia() + 1)
+                grafo.get_lista_vertices()[v].set_predecessor(u)
                 q.append(v)
+            
             i = i + 1
         
-        u.set_cor("preto")
+        grafo.get_lista_vertices()[u].set_cor("preto")
+
+# Imprime os vertices dominantes usando DFS
+def abordagem1(grafo):
+    dfs(grafo)
+    
+    # Versao 1 da abordagem 1, considerava o dominado aquele que possuir um intervalo dentro do intervalo de outro vertice
+    # Ou seja, o dominado tera um intervalo menor do que o do dominador
+    #i = 0
+    #while i < (grafo.get_quantidade_vertices() - 1):
+        #j = i + 1
+        #while j < (grafo.get_quantidade_vertices()):
+            #if grafo.get_lista_vertices()[i].get_distancia() < grafo.get_lista_vertices()[j].get_distancia() and grafo.get_lista_vertices()[i].get_f() > grafo.get_lista_vertices()[j].get_f():
+                #grafo.get_lista_vertices()[j].set_dominante(i)
+            #j = j + 1
+        #i = i + 1
+    
+    # Versao 2 da abordagem 1, considera que o dominador e o prodecessor, pois a DFS retorna a AGM
+    print("Abordagem 1 - DFS")
+    for k in range(grafo.get_quantidade_vertices()):
+        print("Vertice {} - Dominante {}".format(grafo.get_lista_vertices()[k].get_id(), grafo.get_lista_vertices()[k].get_predecessor()))
+
+# Imprime os vertices dominantes usando BFS
+def abordagem2(grafo):
+    bfs(grafo, 0)
     
     print("Abordagem 2 - BFS")
-    for i in range (len(predecessores)):
-        if len(predecessores[i]) > 1:
-            print('Vertice {} - Dominante {}'.format(i, i))
-        elif len(predecessores[i]) == 1:
-            print('Vertice {} - Dominante {}'.format(i, predecessores[i][0]))
-        else:
-            print('Vertice {} - Dominante nulo'.format(i))
+    for i in range(grafo.get_quantidade_vertices()):
+        print("Vertice {} - Dominante {}".format(grafo.get_lista_vertices()[i].get_id(), grafo.get_lista_vertices()[i].get_predecessor()))
 
 # Funcao principal
 def main():
