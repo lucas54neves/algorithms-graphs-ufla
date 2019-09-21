72371ce1fb87d688ed9e930e088ce9d522e5bd7a
Lucas Neves <lucas54neves@gmail.com>
2019-09-21 13:18:06 -0300

    Aprimoramento da classe Grafo

--- algoritmos/grafo.py
+++ algoritmos/grafo.py
@@ -2,30 +2,43 @@ from vertice import Vertice
 
 class Grafo:
     def __init__(self, n):
-        # Cria a lista de adjacencia
+        # Quantidade de vertices
+        self.quantidade_vertices = n
+
+        # Lista de adjacencia
         self.lista_adj = []
         for i in range(n):
             self.lista_adj.append([0] * n)
         
-        # Cria a matriz de adjacencia
+        # Matriz de adjacencia
         self.matriz_adj = []
         for i in range(n):
             self.matriz_adj.append([])
         
-        self.quantidade_vertices = n
-        # Cria a lista de vertices
+        # Lista de vertices para realizar as operacoes
         self.lista_vertices = []
         for i in range(n):
             self.lista_vertices.append(Vertice(i))
     
+    # Retorna a lista de adjacencia
     def get_lista_adj(self):
         return self.lista_adj
     
+    # Retorna a quantidade vertices
     def get_quantidade_vertices(self):
         return self.quantidade_vertices
     
+    # Retorna a lista de vertices
     def get_lista_vertices(self):
         return self.lista_vertices
-    
+
+    # Retorna a matriz de adjacencia
     def get_matriz_adj(self):
         return self.matriz_adj
+
+    # Adiciona aresta direcional
+    def adicionar_aresta_direcional(self, u, v):
+        
+    # Adiciona aresta nao direcinal
+    def adicionar_aresta_direcional(self, u, v):
+        
