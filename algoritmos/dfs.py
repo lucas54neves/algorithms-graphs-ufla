b2ba3b8b32bcc592890cedfb61dde3cfb57f8e97
Lucas Neves <lucas54neves@gmail.com>
2019-10-03 20:31:55 -0300

    Alterações na função de DFS para se adequar às modificações na classe do grafo

--- algoritmos/dfs.py
+++ algoritmos/dfs.py
@@ -1,26 +1,42 @@
+from grafo import *
 
 def dfs(grafo):
     for i in range(grafo.get_quantidade_vertices()):
         grafo.get_lista_vertices()[i].set_cor("branco")
-        grafo.get_lista_vertices()[i].set_predecessor(None)
+        grafo.get_lista_vertices()[i].set_pai(None)
     
     for i in range(grafo.get_quantidade_vertices()):
         if grafo.get_lista_vertices()[i].get_cor() == "branco":
-            dfsVisit(grafo, grafo.get_lista_vertices()[i].get_id())
+            dfs_visit(grafo, i)
     
-def dfsVisit(grafo, id_vertice):
-    global tempo
-    tempo = tempo + 1
+    print("Resultado da Busca em Produndidade")
+    for i in range(grafo.get_quantidade_vertices()):
+        print("Vertice: {} - Tempo inicial: {} - Tempo final: {}".format(grafo.get_lista_vertices()[i].get_indice(), grafo.get_lista_vertices()[i].get_tempo_inicial(), grafo.get_lista_vertices()[i].get_tempo_final()))
+
+def dfs_visit(grafo, indice_vertice):
+    grafo.incrementar_tempo()
     
-    grafo.get_lista_vertices()[id_vertice].set_distancia(tempo)
-    grafo.get_lista_vertices()[id_vertice].set_cor("cinza")
+    grafo.get_lista_vertices()[indice_vertice].set_tempo_inicial(grafo.get_tempo())
+    grafo.get_lista_vertices()[indice_vertice].set_cor("cinza")
     
-    for i in range(len(grafo.get_lista_adj()[id_vertice])):
-        # Pega o id do vertice adjacente e confere os dados desse vertice na lista de adjacencia
-        if grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].get_cor() == "branco":
-            grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].set_predecessor(grafo.get_lista_vertices()[id_vertice].get_id())
-            dfsVisit(grafo, grafo.get_lista_vertices()[grafo.get_lista_adj()[id_vertice][i]].get_id())
+    for adjacente in grafo.get_lista_adjacencia()[indice_vertice]:
+        if grafo.get_lista_vertices()[adjacente].get_cor() == "branco":
+            grafo.get_lista_vertices()[adjacente].set_pai(indice_vertice)
+            dfs_visit(grafo, adjacente)
     
-    grafo.get_lista_vertices()[id_vertice].set_cor("preto")
-    tempo = tempo + 1
-    grafo.get_lista_vertices()[id_vertice].set_f(tempo)
+    grafo.get_lista_vertices()[indice_vertice].set_cor("preto")
+    grafo.incrementar_tempo()
+    grafo.get_lista_vertices()[indice_vertice].set_tempo_final(grafo.get_tempo())
+
+# Funcao principal para teste
+def main():
+    grafo = Grafo(4)
+    grafo.adicionar_aresta_direcional(0, 1)
+    grafo.adicionar_aresta_direcional(0, 3)
+    grafo.adicionar_aresta_direcional(1, 2)
+    grafo.adicionar_aresta_direcional(2, 3)
+    grafo.adicionar_aresta_direcional(3, 1)
+    dfs(grafo)
+
+if __name__ == "__main__":
+    main()
