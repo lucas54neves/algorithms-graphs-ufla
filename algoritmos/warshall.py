d964ee09d659a763360c13ff26567faf1958caa6
Lucas Neves <lucas54neves@gmail.com>
2019-10-04 19:21:38 -0300

    Atualização do Algoritmo de Warshall

--- algoritmos/warshall.py
+++ algoritmos/warshall.py
@@ -1,8 +1,8 @@
-from grafo import Grafo
+from grafo import *
 
 # Algoritmo de Warshall usando a matriz de adjacencia de um grafo
 def warshall (grafo):
     for k in range(grafo.get_quantidade_vertices()):
         for i in range(grafo.get_quantidade_vertices()):
             for j in range(grafo.get_quantidade_vertices()):
-                grafo.get_matriz_adj()[i][j] = int(grafo.get_matriz_adj()[i][j] == 1 or (grafo.get_matriz_adj()[i][k] == 1 and grafo.get_matriz_adj()[k][j] == 1))
+                grafo.get_matriz_adjacencia()[i][j] = int(grafo.get_matriz_adjacencia()[i][j] == 1 or (grafo.get_matriz_adjacencia()[i][k] == 1 and grafo.get_matriz_adjacencia()[k][j] == 1))
