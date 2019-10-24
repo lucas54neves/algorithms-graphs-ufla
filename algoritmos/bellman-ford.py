01d081c90127eff6d9686ba71d74b72a9fc2c583
Lucas Neves <lucas54neves@gmail.com>
2019-10-24 15:54:32 -0300

    Implementação do Algoritmo de Bellman-Ford

--- algoritmos/bellman-ford.py
+++ algoritmos/bellman-ford.py
@@ -5,6 +5,9 @@ class Vertice:
         self.distancia = float('inf')
         self.pai = None
 
+    def get_indice(self):
+        return self.indice
+
     def get_distancia(self):
         return self.distancia
 
@@ -20,15 +23,15 @@ class Vertice:
 # Classe que representa a aresta
 class Aresta:
     def __init__(self, u, v, peso):
-        self.vertice1 = u
-        self.vertice2 = v
+        self.origem = u
+        self.destino = v
         self.peso = peso
 
-    def get_vertice1(self):
-        return self.vertice1
+    def get_origem(self):
+        return self.origem
 
-    def get_vertice2(self):
-        return self.vertice2
+    def get_destino(self):
+        return self.destino
 
     def get_peso(self):
         return self.peso
@@ -40,18 +43,53 @@ class Grafo:
         self.lista_vertices = []
         for i in range(self.quantidade_vertices):
             self.lista_vertices.append(Vertice(i))
-        self.lista_arestas = []
+        self.lista_adjacencia = []
+        for i in range(self.quantidade_vertices):
+            self.lista_adjacencia.append([])
 
-    #def relax(self, u, v, aresta):
-    #    if self.lista_vertices[v].get_distancia() > self.lista_vertices[u].get_distancia() + self.lista_arestas[are]
+    def relax(self, u, v):
+        if self.lista_vertices[v].get_distancia() > self.lista_vertices[u].get_distancia() + self.retornar_peso(u, v):
+            self.lista_vertices[v].set_distancia(self.lista_vertices[u].get_distancia() + self.retornar_peso(u, v))
+            self.alterar_pai(v, u)
 
     def retornar_peso(self, u, v):
-        indice = self.lista_arestas.index(key=lambda x: x.get_vertice1() == u and x.get_vertice2() == v)
-        return self.lista_arestas[indice].get_peso()
+        for adjacente in self.lista_adjacencia[u]:
+            if adjacente.get_destino() == v:
+                return adjacente.get_peso()
+
+    def alterar_distancia(self, u, v):
+        self.lista_vertices[v].set_distancia(self.lista_vertices[u].get_distancia() + self.retornar_peso(u, v))
+
+    def alterar_pai(self, u, novo_pai):
+        self.lista_vertices[u].set_pai(novo_pai)
 
     def adicionar_aresta(self, u, v, peso):
-        self.lista_arestas.append(Aresta(u, v, peso))
+        self.lista_adjacencia[u].append(Aresta(u, v, peso))
 
-grafo = Grafo(3)
-grafo.adicionar_aresta(1,2, 4.5)
-print(grafo.retornar_peso(1, 2))
+    def bellman_ford(self):
+        self.lista_vertices[0].set_distancia(0)
+        for i in range(self.quantidade_vertices - 1):
+            for aresta in self.lista_adjacencia[i]:
+                self.relax(aresta.get_origem(), aresta.get_destino())
+        for aresta in self.lista_adjacencia[i]:
+            if self.lista_vertices[aresta.get_destino()].get_distancia() > self.lista_vertices[aresta.get_origem()].get_distancia() + self.retornar_peso(aresta.get_origem(), aresta.get_destino()):
+                return False
+        return True
+
+    def imprimir_resultado(self):
+        for i in self.lista_vertices:
+            print("{}: {}".format(i.get_indice(), i.get_distancia()))
+
+grafo = Grafo(5)
+grafo.adicionar_aresta(0,1,6)
+grafo.adicionar_aresta(0,2,7)
+grafo.adicionar_aresta(1,2,8)
+grafo.adicionar_aresta(1,3,5)
+grafo.adicionar_aresta(1,4,-4)
+grafo.adicionar_aresta(2,3,-3)
+grafo.adicionar_aresta(2,4,9)
+grafo.adicionar_aresta(3,2,-2)
+grafo.adicionar_aresta(4,0,2)
+grafo.adicionar_aresta(4,3,7)
+print(grafo.bellman_ford())
+grafo.imprimir_resultado()
