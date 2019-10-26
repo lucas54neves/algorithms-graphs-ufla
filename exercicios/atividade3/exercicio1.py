cdcdb707ab03ef5c7bc726548052928e69f01a34
Lucas Neves <lucas54neves@gmail.com>
2019-10-26 18:32:26 -0300

    Classes para representar o grafo e o vertice adjacente

--- exercicios/atividade3/exercicio1.py
+++ exercicios/atividade3/exercicio1.py
@@ -1,15 +1,54 @@
-def leitura(nome_arquivo):
-    arquivo = open(nome_arquivo, "r")
+# Classe que representa o vertice adjacente
+class Adjacente:
+    def __init__(self, i):
+        # Indice do vertice adjacente
+        self.indice = i
+        # Atributo que informa se a aresta foi visitada
+        self.visitada = False
 
-    #numero_vertices = int(arquivo.readline())
+    def get_indice(self):
+        return self.indice
 
-    #grafo = Grafo(numero_vertices)
+    def get_visitada(self):
+        return self.visitada
 
-    for linha in arquivo:
-        valores = linha.split()
-        #grafo.adicionar_aresta(int(valores[0]), int(valores[1]))
+    def visitar(self):
+        self.visitada = True
 
-        # Testa se os valores foram lidos
-        print(valores)
+# Classe que representa o grafo
+class Grafo:
+    def __init__(self, nome_arquivo):
+        arquivo = open(nome_arquivo, "r")
+        # Quantidade de vertices
+        self.quantidade_vertices = int(arquivo.readline())
+        # Lista de adjacencia dos vertices
+        self.lista_adjacencia = []
+        for i in range(self.quantidade_vertices):
+            self.lista_adjacencia.append([])
+        # Adiciona as arestas apartir do arquivo
+        for linha in arquivo:
+            valores = linha.split()
+            self.adicionar_aresta(int(valores[0]), int(valores[1]))
 
-leitura("Ex1.txt")
+    def adicionar_aresta(self, u, v):
+        self.lista_adjacencia[u].append(Adjacente(v))
+
+    def hierholzer(self):
+        # Vertice inicial do algoritmo
+        # Escolhido aleatoriamente
+        vertice_inicial = random.randint(0, self.quantidade_vertices)
+
+        # Armazena o circuito
+        circuito = []
+
+        while circuito:
+            pass
+
+    # Metodo para imprimir o resultado de acordo com o que pede no enunciado
+    # O circuito euleriano ou a mensagem "Grafo Nao Euleriano"
+    def imprimir_resultado(self):
+        pass
+
+grafo = Grafo("Ex1.txt")
+#grafo.hierholzer()
+#grafo.imprimir_resultado()
