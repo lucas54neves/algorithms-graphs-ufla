9eb7ffa175e6e0f1640a57b436a8fd9dfceb0da0
Lucas Neves <lucas54neves@gmail.com>
2019-11-07 18:02:02 -0300

    Versão 5 da questão 4

--- exercicios/atividade3/exercicio4.py
+++ exercicios/atividade3/exercicio4.py
@@ -1,12 +1,24 @@
 import math
 
 class Vertice:
-    # Construtor do vertice
-    # x = coordenada x
-    # y = coordenada y
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.tempo = float("inf")
+        self.pai = None
+        self.arestas = []
+
+    def relax(self, aresta):
+        self.tempo = aresta.adjacente.tempo + aresta.peso
+        self.pai = adjacente
+
+    def adicionar_aresta(self, destino, peso):
+        self.arestas.append(Aresta(destino, peso))
+
+class Aresta:
+    def __init__(self, destino, peso):
+        self.destino = destino
+        self.peso = peso
 
 class Linha:
     def __init__(self):
@@ -18,17 +30,18 @@ class Linha:
 class Grafo:
     def __init__(self, nome_arquivo):
         arquivo = open(nome_arquivo, 'r')
+        self.vertices = []
 
         linha = arquivo.readline()
         valores = linha.split()
 
-        casa = Vertice(int(valores[0]), int(valores[1]))
-        escola = Vertice(int(valores[2]), int(valores[3]))
+        self.vertices.append([Vertice(int(valores[0]), int(valores[1])), 0])
+        self.vertices.append([Vertice(int(valores[2]), int(valores[3])), 0])
 
         linha = arquivo.readline()
 
         # Leitura das coordenadas das linhas de metro
-        linhas = []
+        i = 1
         while linha:
             valores = linha.split()
             linha_metro = Linha()
@@ -38,46 +51,21 @@ class Grafo:
                 coordenada_y = int(valores.pop(0))
 
                 if coordenada_x != -1 and coordenada_y != -1:
-                    linha_metro.adicionar_estacoes(Vertice(coordenada_x, coordenada_y))
+                    self.vertices.append([Vertice(coordenada_x, coordenada_y), i])
 
-            linhas.append(linha_metro)
-
+            i += 1
             linha = arquivo.readline()
 
-        soma = 0
-        for linha in linhas:
-            soma += len(linha.estacoes)
-
-        self.n = 2 + soma
-
-        self.matriz = []
-        for i in range(self.n):
-            linha = []
-            for j in range(self.n):
-                linha.append(0)
-            self.matriz.append(linha)
-
-        arestas = []
-        arestas.append([casa, 0])
-
-        i = 1
-        for linha in linhas:
-            for estacao in linha.estacoes:
-                arestas.append([estacao, i])
-            i += 1
-
-        arestas.append([escola, 0])
-
         i = 0
-        for aresta1 in arestas:
+        for vertice1 in self.vertices:
             j = 0
-            for aresta2 in arestas:
-                if aresta1[1] == 0:
-                    self.matriz[i][j] = self.calcular_tempo(aresta1[0], aresta2[0], 1.7)
-                elif aresta1[1] == aresta2[1]:
-                    self.matriz[i][j] = self.calcular_tempo(aresta1[0], aresta2[0], 3.1111111)
+            for vertice2 in self.vertices:
+                if vertice1[1] == 0:
+                    vertice1[0].adicionar_aresta(vertice2, self.calcular_tempo(vertice1[0], vertice2[0], 1.7))
+                elif vertice1[1] == vertice2[1]:
+                    vertice1[0].adicionar_aresta(vertice2, self.calcular_tempo(vertice1[0], vertice2[0], 3.1111111111))
                 else:
-                    self.matriz[i][j] = self.calcular_tempo(aresta1[0], aresta2[0], 1.7)
+                    vertice1[0].adicionar_aresta(vertice2, self.calcular_tempo(vertice1[0], vertice2[0], 1.7))
                 j += 1
             i += 1
 
@@ -87,21 +75,31 @@ class Grafo:
     def calcular_tempo(self, vertice1, vertice2, velocidade):
         return math.ceil((self.calcular_distancias(vertice1, vertice2) / velocidade) / 60)
 
-    def imprimir_matriz(self):
-        for i in range(len(self.matriz)):
-            print(self.matriz[i])
+    def imprimir_resultado(self):
+        print(self.vertices[len(self.vertices) - 1].tempo)
 
-    def floyd_warshall(self):
-        for k in range(self.n):
-            for i in range(self.n):
-                for j in range(self.n):
-                    if self.matriz[i][j] > self.matriz[i][k] + self.matriz[k][j]:
-                        self.matriz[i][j] = self.matriz[i][k] + self.matriz[k][j]
+    def dijkstra(self):
+        self.inicializar_vertices()
+        
+        self.vertices[0][0].tempo = 0
+
+        conjunto_q = self.vertices
+        
+        while len(conjunto_q) != 0:
+            u = min(conjunto_q, key=lambda v: v[0].tempo)
+            for aresta in u[0].arestas:
+                if u[0].tempo > aresta.destino[0].tempo + aresta.peso:
+                    u[0].relax(aresta)
+
+    def inicializar_vertices(self):
+        for vertice in self.vertices:
+            vertice[0].tempo = float("inf")
+            vertice[0].pai = None
 
 def main():
     grafo = Grafo("Ex3.txt")
-    grafo.floyd_warshall()
-    grafo.imprimir_matriz()
+    grafo.dijkstra()
+    grafo.imprimir_resultado()
 
 if __name__ == "__main__":
     main()
